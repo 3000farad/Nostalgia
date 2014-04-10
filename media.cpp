79db803823b2143ebfd2da500eca7f2a39b22644
Jason Prince <me@3000farad.com>
2014-04-10 02:03:31 -0600

    License headers added

--- media.cpp
+++ media.cpp
@@ -1,3 +1,23 @@
+/*********************************************************
+*  This file is part of Nostalgia.                       *
+*                                                        *
+*  Nostalgia is free software: you can redistribute it   *
+*  and/or modify it under the terms of the GNU General   *
+*  Public License as published by the Free Software      *
+*  Foundation, either version 3 of the License, or (at   *
+*  your option) any later version.                       *
+*                                                        *
+*  Nostalgia is distributed in the hope that it will be  *
+*  useful, but WITHOUT ANY WARRANTY; without even the    *
+*  implied warranty of MERCHANTABILITY or FITNESS FOR A  *
+*  PARTICULAR PURPOSE.  See the GNU General Public       *
+*  License for more details.                             *
+*                                                        *
+*  You should have received a copy of the GNU General    *
+*  Public License along with Nostalgia.  If not, see     *
+*  <http://www.gnu.org/licenses/>.                       *
+*********************************************************/
+
 #include "media.h"
 
 Media::Media(quint32 m_id) {
@@ -8,5 +28,5 @@ Media::Media(quint32 m_id) {
 quint32 Media::getID() { return id; }
 QString Media::getName() { return name; }
 QString Media::getLocation() { return location; }
-bool Media::getFindFailed() { return location; }
-bool Media::getReadFailed() { return location; }
+bool Media::getFindFailed() { return findFailed; }
+bool Media::getReadFailed() { return readFailed; }
